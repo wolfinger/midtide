79867095e9abba220eae2da75621c57783d53ecc
greg wolfinger <greg@wolfinger.io>
2022-03-19 16:08:39 -0700

    cal update

--- midtide/cal.py
+++ midtide/cal.py
@@ -14,14 +14,16 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 def set_credentials():
     """
-    set google calendar credentials
+    set google calendar credentials, code snippet lifted from google cal api start docs
     """
     creds = None
+    
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
     if os.path.exists('data/token.json'):
         creds = Credentials.from_authorized_user_file('data/token.json', SCOPES)
+    
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -30,6 +32,7 @@ def set_credentials():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'data/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
+        
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
